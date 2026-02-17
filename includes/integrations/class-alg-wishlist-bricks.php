7168128c9271fb3269285c11fe28e418a58b0207
Alejandro Aruca <alearuk2@gmail.com>
2026-02-17 17:26:49 -0500

    fix: v1.0.8 - Restore Bricks Query and Elements

--- includes/integrations/class-alg-wishlist-bricks.php
+++ includes/integrations/class-alg-wishlist-bricks.php
@@ -10,15 +10,16 @@ class Alg_Wishlist_Bricks
 
     public static function init()
     {
+        // Load Query Integration immediately to catch setup/control_options hooks early
+        require_once ALG_WISHLIST_PATH . 'includes/integrations/class-alg-wishlist-bricks-query.php';
+        Alg_Wishlist_Bricks_Query::init();
+
+        // Register Elements
         add_action('bricks/elements/register', array(__CLASS__, 'register_elements'), 11);
     }
 
     public static function register_elements()
     {
-        // Load Query Integration
-        require_once ALG_WISHLIST_PATH . 'includes/integrations/class-alg-wishlist-bricks-query.php';
-        Alg_Wishlist_Bricks_Query::init();
-
         // Register Wishlist Button Element
         \Bricks\Elements::register_element(ALG_WISHLIST_PATH . 'includes/integrations/elements/wishlist-button.php');
 
