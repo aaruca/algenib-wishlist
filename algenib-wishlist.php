705f776848cd327ce1229b55cd55705062c0aa2b
Alejandro Aruca <alearuk2@gmail.com>
2026-02-17 13:25:23 -0500

    Chore: Bump version to 1.0.4

--- algenib-wishlist.php
+++ algenib-wishlist.php
@@ -3,7 +3,7 @@
  * Plugin Name: Algenib Wishlist
  * Plugin URI:  https://algenib.io
  * Description: A complete, high-performance Wishlist plugin with native Bricks Builder integration and Doofinder compatibility.
- * Version:     1.0.0
+ * Version:     1.0.4
  * Author:      Algenib
  * Author URI:  https://algenib.io
  * Text Domain: algenib-wishlist
@@ -12,16 +12,16 @@
  * @package Algenib_Wishlist
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
 // Define Constants
-define( 'ALG_WISHLIST_VERSION', '1.0.0' );
-define( 'ALG_WISHLIST_FILE', __FILE__ );
-define( 'ALG_WISHLIST_PATH', plugin_dir_path( __FILE__ ) );
-define( 'ALG_WISHLIST_URL', plugin_dir_url( __FILE__ ) );
-define( 'ALG_WISHLIST_BASENAME', plugin_basename( __FILE__ ) );
+define('ALG_WISHLIST_VERSION', '1.0.4');
+define('ALG_WISHLIST_FILE', __FILE__);
+define('ALG_WISHLIST_PATH', plugin_dir_path(__FILE__));
+define('ALG_WISHLIST_URL', plugin_dir_url(__FILE__));
+define('ALG_WISHLIST_BASENAME', plugin_basename(__FILE__));
 
 /**
  * The Switchboard Class
@@ -31,25 +31,28 @@ require_once ALG_WISHLIST_PATH . 'includes/class-alg-wishlist-loader.php';
 /**
  * Activation Hook
  */
-function activate_algenib_wishlist() {
+function activate_algenib_wishlist()
+{
 	require_once ALG_WISHLIST_PATH . 'includes/class-alg-wishlist-activator.php';
 	Alg_Wishlist_Activator::activate();
 }
-register_activation_hook( __FILE__, 'activate_algenib_wishlist' );
+register_activation_hook(__FILE__, 'activate_algenib_wishlist');
 
 /**
  * Deactivation Hook
  */
-function deactivate_algenib_wishlist() {
+function deactivate_algenib_wishlist()
+{
 	require_once ALG_WISHLIST_PATH . 'includes/class-alg-wishlist-deactivator.php';
 	Alg_Wishlist_Deactivator::deactivate();
 }
-register_deactivation_hook( __FILE__, 'deactivate_algenib_wishlist' );
+register_deactivation_hook(__FILE__, 'deactivate_algenib_wishlist');
 
 /**
  * Begin Execution
  */
-function run_algenib_wishlist() {
+function run_algenib_wishlist()
+{
 	$plugin = new Alg_Wishlist_Loader();
 	$plugin->run();
 }
